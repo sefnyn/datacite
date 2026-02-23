aecce5ebdfd53345e70ad853cfc5c22b97ee2a9c
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-23 11:23:35 +0000

    Update get_all_dois.py

--- get_all_dois.py
+++ get_all_dois.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.datacite.org/dois?prefix=10.15128?fields[dois]=titles&page[size]=10000&sort=name"
+url = "https://api.datacite.org/dois?prefix=10.15128&fields[dois]=creators,titles&page[size]=10000&sort=name"
 
 headers = {"accept": "text/csv"}
 
