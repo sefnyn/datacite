e35ef329678d019fd0882dbed634b1776186b5f1
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-24 10:24:41 +0000

    Update get_all_dois.py

--- get_all_dois.py
+++ get_all_dois.py
@@ -12,7 +12,7 @@ print('Writing to file...')
 fh.write(response.text)
 
 print('Getting next page...')
-next = "https://api.datacite.org/dois?page[number]=1&page[size]=1000&prefix=10.15128"
+next = "https://api.datacite.org/dois?page[number]=2&page[size]=1000&prefix=10.15128"
 response = requests.get(next, headers=headers)
 fh = open (out, 'a')
 fh.write(response.text)
