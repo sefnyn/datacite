b0275d7eec2b96a6e1182bc5aa913a1310956558
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-26 12:06:31 +0000

    Update get_doi_from_json.py

--- get_doi_from_json.py
+++ get_doi_from_json.py
@@ -1,3 +1,14 @@
-json = "dois.json"
-fh = open (json)
+import requests
+#json = 'dois.json'
+#fh2 = open (json, 'w')
+url = "https://api.datacite.org/dois?prefix=10.15128&page[size]=1000&sort=name"
 
+#get json data
+headers = {"accept": "application/vnd.api+json"}
+print('Getting JSON metadata from DataCite...')
+response = requests.get(url, headers=headers)
+
+#read json data
+data = response.json()['data']
+first = data[0]
+print(first)
