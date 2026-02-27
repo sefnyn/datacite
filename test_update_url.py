598539ec061bfc528855fbcd4bae7152ba7d9722
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-27 14:04:55 +0000

    Create test_update_url.py

---
+++ test_update_url.py
@@ -0,0 +1,16 @@
+import requests
+payload =
+{
+  "data": {
+    "type": "dois",
+    "attributes": {
+      "url": "https://example.org"
+    }
+  }
+}
+
+def test_update(doi):
+  rest_api = "https://api.test.datacite.org/dois/" + doi
+  user = "uzah"
+  headers = {"accept" : "application/vnd.api+json"}
+  
