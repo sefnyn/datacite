d0875a0875e7e24e8768a8f7a13134b7fffdc3c9
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-27 14:38:39 +0000

    Update test_update_url.py

--- test_update_url.py
+++ test_update_url.py
@@ -1,16 +1,22 @@
 import requests
 payload =
 {
-  "data": {
-    "type": "dois",
+    "data": {
+        "type": "dois",
     "attributes": {
-      "url": "https://example.org"
+        "url": "https://example.org"
     }
   }
 }
 
 def test_update(doi):
-  rest_api = "https://api.test.datacite.org/dois/" + doi
-  user = "uzah"
-  headers = {"accept" : "application/vnd.api+json"}
-  
+    rest_api = "https://api.test.datacite.org/dois/" + doi
+    user = "uzah"
+    headers = 
+    {
+        "accept" : "application/vnd.api+json",
+        "content-type": "application/json"
+    }  
+    response = requests.put(rest_api, json=payload, headers=headers)
+
+
