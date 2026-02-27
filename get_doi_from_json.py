4e3abbc2183d7f2d3bbd5ea19dc445f46b6abd00
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-02-27 11:27:31 +0000

    Update get_doi_from_json.py

--- get_doi_from_json.py
+++ get_doi_from_json.py
@@ -23,7 +23,7 @@ def get_dois(prefix):
         else:
 #            print(rec['id'] + "," + str(rec['attributes']['publicationYear']) + "," + rec['attributes']['url'])
             fh.write(rec['id'] + "," + str(rec['attributes']['publicationYear']) + "," + rec['attributes']['url'] + "\n")
-    print("Found {num} DOIs with prefix {prefix}".format(num, prefix))
+    print("Found {0} DOIs with prefix {1}".format(num, prefix))
 
 def main():
     try:
