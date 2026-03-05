8413146a6903fabaa40887a53b88315fc5360fb2
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-03-05 16:30:46 +0000

    Update test_batch_lp_update.py

--- test_batch_lp_update.py
+++ test_batch_lp_update.py
@@ -8,12 +8,16 @@ test_update_landing_page.test_update([SHORT_DOI], [LANDING_PAGE])
 import test_update_landing_page
 import getpass
 
-def update_landing_pages(dois, new_lp):
+def update_landing_pages(dois, server):
     # Get password
     pwd = getpass.getpass(prompt="Enter password for DataCite user (N.B.: you will *not* see any input as you type): ", stream=None)
     fh = open(dois)
     for doi in fh:
         doi = doi.rstrip()
+        parts = doi.strip("/")
+        prefix = parts[0]
+        suffix = parts[1]
+        new_lp = server + "/files/" + suffix
         test_update_landing_page.test_update(pwd, doi, new_lp)
 
 
@@ -21,7 +25,7 @@ def main():
     try:
         update_landing_pages(sys.argv[1], sys.argv[2])
     except IndexError:
-        print("Usage:\n", sys.argv[0], "[FILE_CONTAINING_SHORT_DOIS] [NEW_LANDING_PAGE_WITHOUT_RECORD_ID]")
+        print("Usage:\n", sys.argv[0], "[FILE_CONTAINING_SHORT_DOIS] [NEW_SERVER_NAME]")
 
 if __name__ == "__main__":
     import sys
