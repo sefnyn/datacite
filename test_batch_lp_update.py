ad3c7409bc7b1331129a2707913d6fb78179e31c
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-03-05 14:40:13 +0000

    Update test_batch_lp_update.py

--- test_batch_lp_update.py
+++ test_batch_lp_update.py
@@ -1,5 +1,8 @@
 import test_update_landing_page
 
 """
+This module calls function **test_update** in another module:
 
-test_update_landing_page.test_update("10.4124/R90R967372K","https://collections-test.durham.ac.uk/files/r90r967372k")
+test_update_landing_page.test_update([SHORT_DOI], [LANDING_PAGE])
+"""
+
