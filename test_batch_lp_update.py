5fea957eabed4ebc8976041aba407e6d57ed82cf
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-03-06 12:38:53 +0000

    Update test_batch_lp_update.py

--- test_batch_lp_update.py
+++ test_batch_lp_update.py
@@ -8,9 +8,9 @@ test_update_landing_page.test_update([SHORT_DOI], [LANDING_PAGE])
 import test_update_landing_page
 import getpass
 
-def update_landing_pages(dois, server):
+def update_landing_pages(user, dois, server):
     # Get password
-    pwd = getpass.getpass(prompt="Enter password for DataCite user (N.B.: you will *not* see any input as you type): ", stream=None)
+    pwd = getpass.getpass(prompt="Enter password for user " + user " + " (N.B.: you will *not* see any input as you type): ", stream=None)
     fh = open(dois)
     for doi in fh:
         doi = doi.rstrip()
@@ -18,14 +18,14 @@ def update_landing_pages(dois, server):
         prefix = parts[0]
         suffix = parts[1]
         new_lp = server + "/files/" + suffix
-        test_update_landing_page.test_update(pwd, doi, new_lp)
+        test_update_landing_page.test_update(user, pwd, doi, new_lp)
 
 
 def main():
     try:
-        update_landing_pages(sys.argv[1], sys.argv[2])
+        update_landing_pages(sys.argv[1], sys.argv[2], sys.argv[3])
     except IndexError:
-        print("Usage:\n", sys.argv[0], "[FILE_CONTAINING_SHORT_DOIS] [NEW_SERVER_NAME]")
+        print("Usage:\n", sys.argv[0], "[DATACITE_USER] [FILE_CONTAINING_SHORT_DOIS] [NEW_SERVER_NAME]")
 
 if __name__ == "__main__":
     import sys
