49f909d0f94c2fb50520ac9739dc13f103744491
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-06-29 11:53:03 +0100

    Create fix_contributor_type.py

---
+++ fix_contributor_type.py
@@ -0,0 +1,9 @@
+"""
+Need to fix this error:
+{"errors":[{"source":"xml","title":"DOI 10.15128/1544bp08d: No matching global declaration available for the validation root. at line 2, column 0","uid":"10.15128/1544bp08d"},{"source":"contributors","title":"Contributor '{\"name\"=\u003e\"Durham University\", \"affiliation\"=\u003e[]}' is missing a required element: contributor type.","uid":"10.15128/1544bp08d"}]}
+
+Contributor Type is missing from 100s of records.
+"""
+
+
+ 
