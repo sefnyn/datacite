71379ab90c4dc5bf59bdc8b1dd935cb98b7e9e74
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-07-09 12:49:14 +0100

    Update get_repository_ids.py

--- get_repository_ids.py
+++ get_repository_ids.py
@@ -1,2 +1,17 @@
-file = 'dois_resolve2sufia.txt'
-open
+def read(file):
+  fh = open(file)
+  fh2 = open('sufia_ids.txt', 'w')
+  for line in fh:
+      
+
+
+
+def main():
+    try:
+        read(sys.argv[1])
+    except IndexError:
+        print("Usage:\n", sys.argv[0], "file containing DOIs")
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
