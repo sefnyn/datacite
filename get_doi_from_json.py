e1c6e830d0228215d4ebb9b32f7609e89ec3edc3
Nicholas Syrotiuk <sefnyn@gmail.com>
2026-07-10 11:13:02 +0100

    Update get_doi_from_json.py

--- get_doi_from_json.py
+++ get_doi_from_json.py
@@ -1,10 +1,12 @@
 import requests
 
 def get_dois(prefix):
-    dois = 'dois_resolve2sufia.txt'
-    fh = open(dois, 'w')
+    coll = 'dois_resolve2collections.txt'
+    fh1 = open(coll, 'w')
+    rd = 'dois_resolve2researchdata.txt'
+    fh2 = open(rd, 'w')
     other = 'dois_resolve_elsewhere.txt'
-    fh2 = open(other, 'w')
+    fh3 = open(other, 'w')
     url = "https://api.datacite.org/dois?prefix=" + prefix + "&page[size]=1000&sort=name"
 
     #get json data
@@ -14,26 +16,28 @@ def get_dois(prefix):
 
     #read json data
     print("Reading JSON metadata...")
-    sufia_count = 0
+    rd_count = 0
+    coll_count = 0
     other_count = 0
     data = response.json()['data']
     for rec in data:
         url = rec['attributes']['url']
-#        f = url.find('collections.durham.ac.uk')
+        f = url.find('collections.durham.ac.uk')
         g = url.find('researchdata.durham.ac.uk')
-#        if f > 0:
-#            sufia_count += 1
-#            print(rec['id'] + "," + str(rec['attributes']['publicationYear']))
-#            fh.write(rec['id'] + "\n")
+        if f > 0:
+            coll_count += 1
+            print(rec['id'] + "," + str(rec['attributes']['publicationYear']))
+            fh1.write(rec['id'] + "\n")
         if g > 0:
-            sufia_count += 1
-            fh.write(rec['id'] + "\n")            
+            rd_count += 1
+            fh2.write(rec['id'] + "\n")            
         else:
             other_count += 1
 #            print(rec['id'] + "," + str(rec['attributes']['publicationYear']) + "," + rec['attributes']['url'])
-            fh2.write(rec['id'] + "," + rec['attributes']['url'] + "\n")
-    print("Found {0} DOIs with prefix {1}".format(sufia_count + other_count, prefix))
-    print("Created new file with " + str(sufia_count) + " DOIs that resolve to Sufia server: " + dois)
+            fh3.write(rec['id'] + "," + rec['attributes']['url'] + "\n")
+    print("Found {0} DOIs with prefix {1}".format(coll_count + rd_count + other_count, prefix))
+    print("Created new file with " + str(coll_count) + " DOIs that resolve to collections: " + coll)
+    print("Created new file with " + str(rd_count) + " DOIs that resolve to researchdata: " + rd)
     print("Created new file with " + str(other_count) + " DOIs that resolve elsewhere: " + other)
 
 
